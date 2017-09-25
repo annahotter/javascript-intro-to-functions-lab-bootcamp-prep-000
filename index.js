40a56b5f3747f772ee18a60b2ce97f167b993c01
Anna Hotter <annajhotter@gmail.com>
2017-09-25 01:50:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,12 @@ function sayHiToGrandma(string) {
 
   if (string.toUpperCase() === string) {return "YES INDEED!"}
   if (string === "I love you, Grandma.") {return "I love you, too."} }
+
+  function logShout(string) {
+    console.log(string.toUpperCase)
+  }
+
+  function logWhisper(string) {
+    console.log(string.toLowerCase)
+  }
   
