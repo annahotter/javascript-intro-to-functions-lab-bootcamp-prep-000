57cd1deb402e503343fc2c7e43cae56e5f3424f2
Anna Hotter <annajhotter@gmail.com>
2017-09-25 01:27:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function sayHiToGrandma(string) { return string.toLowerCase()}
+function sayHiToGrandma(string) { console.log("YES INDEED!")}
+
